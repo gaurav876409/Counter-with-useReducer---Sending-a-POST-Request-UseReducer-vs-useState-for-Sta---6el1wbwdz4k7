070c92a268cb28bf63ad17886b95991126637600
gaurav876409 <gaurav876409@gmail.com>
2023-06-10 22:14:15 +0530

    up

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,5 +1,12 @@
 const counterReducer = (state,action) =>{
-
-}
+    switch (action.type){
+        case 'increment':
+        return state + 1;
+        case 'decrement':
+            return state - 1;
+            default:
+            return state;
+    };
+};
 
 export {counterReducer}
